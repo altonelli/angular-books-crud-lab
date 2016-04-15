efeb9ae5ed98b6e3c8f34cb864d9d9d4a2936016
altonelli <altonelli14@gmail.com>
2016-04-15 15:03:45 -0700

    Finished Weekend lab. Need to do stretch.

--- controllers/BooksIndexController.js
+++ controllers/BooksIndexController.js
@@ -4,4 +4,27 @@ angular.module('libraryApp')
 BooksIndexController.$inject=['$http'];
 function BooksIndexController( $http ) {
   var vm = this;
+
+  vm.newBook = {};
+
+  $http({
+    method: 'GET',
+    url: 'https://super-crud.herokuapp.com/books'
+  }).then(function successCallback(res){
+    vm.books = res.data.books;
+  }, function errorCallback(err){
+    console.log(err);
+  });
+
+  vm.createBook = function(){
+    $http({
+      method: 'POST',
+      url: 'https://super-crud.herokuapp.com/books/',
+      data: vm.newBook
+    }).then(function successCallback(res){
+      console.log(res.data);
+      vm.books.push(res.data);
+    });
+  };
+
 }
--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -1,7 +1,39 @@
 angular.module('libraryApp')
-  .controller('BooksIndexController', BooksIndexController);
+  .controller('BooksShowController', BooksShowController);
 
 BooksShowController.$inject=['$http', '$routeParams', '$location'];
 function BooksShowController($http, $routeParams, $location) {
   var vm = this;
+
+
+  vm.showBook = function(){
+    $http({
+      method: 'GET',
+      url: 'https://super-crud.herokuapp.com/books/' + $routeParams.id
+    }).then(function successCallback(res){
+      vm.book = res.data;
+    });
+  };
+
+  vm.showBook();
+
+  vm.deleteBook = function(){
+    $http({
+      method: 'DELETE',
+      url: 'https://super-crud.herokuapp.com/books/' + $routeParams.id
+    }).then(function successCallback(res){
+      $location.path('/');
+    });
+  };
+
+  vm.editBook = function(){
+    $http({
+      method: 'PUT',
+      url: 'https://super-crud.herokuapp.com/books/' + $routeParams.id,
+      data: vm.book
+    }).then(function successCallback(res){
+      $location.path('/');
+    });
+  };
+
 };
